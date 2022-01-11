5ace5039dfceba7bab88ab4ef42c720a3f3e80a8
NongQ0689 <suraphat.q@gmail.com>
2022-01-11 16:46:14 +0700

    finish

--- lab15_2.cpp
+++ lab15_2.cpp
@@ -19,3 +19,46 @@ int main(){
 	showMatrix(B);
 	return 0;
 }
+
+
+void inputMatrix(double input[][N])
+{
+	for(int i=0 ; i<N ;i++)
+	{
+		cout<<"Row "<<i+1<<": ";
+		for(int j=0; j<N ; j++)
+		{
+			cin>>input[i][j];
+		}
+	}
+}
+
+void showMatrix(const bool Show[][N])
+{
+	for(int i=0 ; i<N ;i++)
+	{
+		for(int j=0; j<N ; j++)
+		{
+			cout<<Show[i][j]<<" ";
+		}
+		cout<<"\n";
+	}
+}
+
+void findLocalMax(const double IP[][N], bool OP[][N])
+{
+
+	for(int i=0 ; i<N ;i++)
+	{
+		for(int j=0; j<N ; j++) OP[i][j]=false;
+	}
+
+	for(int i=1 ; i<N-1 ;i++)
+	{
+		for(int j=1; j<N-1 ; j++)
+		{
+			if( IP[i][j] >= IP[i-1][j] && IP[i][j] >= IP[i+1][j] && IP[i][j] >= IP[i][j-1] && IP[i][j] >= IP[i][j+1] )
+				 OP[i][j]=true;
+		}
+	}
+}
